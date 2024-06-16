f77e2eff7275cc8493641f5d6a3ce558d4c73fe7
rafimhathufa <rafimhathufa@yahoo.com>
2024-06-16 07:08:19 +0700

    Bugfixing db.js change serviceaccount

--- database/db.js
+++ database/db.js
@@ -1,4 +1,3 @@
-// db.js
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 
@@ -7,23 +6,6 @@ dotenv.config();
 
 const initializeFirestoreAndAuth = () => {
   try {
-    // Debug prints
-    console.log("GOOGLE_CLOUD_KEYFILE:", process.env.GOOGLE_CLOUD_KEYFILE ? "Loaded" : "Not Set");
-    console.log("FIREBASE_DATABASE_URL:", process.env.FIREBASE_DATABASE_URL);
-
-    // Check if GOOGLE_CLOUD_KEYFILE environment variable is set
-    if (!process.env.GOOGLE_CLOUD_KEYFILE) {
-      throw new Error('GOOGLE_CLOUD_KEYFILE environment variable is not set.');
-    }
-
-    // Parse the service account key
-    let serviceAccountKey;
-    try {
-      serviceAccountKey = JSON.parse(process.env.GOOGLE_CLOUD_KEYFILE);
-    } catch (error) {
-      throw new Error('Error parsing GOOGLE_CLOUD_KEYFILE JSON: ' + error.message);
-    }
-
     // Check if FIREBASE_DATABASE_URL environment variable is set
     if (!process.env.FIREBASE_DATABASE_URL) {
       throw new Error('FIREBASE_DATABASE_URL environment variable is not set.');
@@ -31,9 +13,9 @@ const initializeFirestoreAndAuth = () => {
 
     const databaseURL = process.env.FIREBASE_DATABASE_URL;
 
-    // Initialize Firebase Admin SDK
+    // Initialize Firebase Admin SDK using Application Default Credentials
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccountKey),
+      credential: admin.credential.applicationDefault(),
       databaseURL: databaseURL
     });
 
@@ -47,4 +29,4 @@ const initializeFirestoreAndAuth = () => {
   }
 };
 
-module.exports = initializeFirestoreAndAuth()
+module.exports = initializeFirestoreAndAuth();
