e2684a41f2bf29e38b37b3eb585fc525cd63d8ca
nipoji <hanifnurfauzi1@gmail.com>
2024-06-19 01:17:51 +0700

    Update Profile routes in app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// app.js
+//app.js
 "use strict";
 const express = require("express");
 const userRoutes = require("./src/user-routes");
 const activityRoutes = require("./src/activity-routes");
 const reportRoutes = require("./src/report-routes");
+const profileRoutes = require("./src/profile-routes");
 require('dotenv').config();
 
 const app = express();
@@ -15,13 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("This is EmoQu Backend with Cloud Run!");
+  res.send("This is EmoQu Backend");
 });
 
 //routes
 app.use("/auth", userRoutes.routes);
 app.use("/api", activityRoutes.routes);
 app.use("/api", reportRoutes.routes);
+app.use("/api", profileRoutes.routes);
 
 //port
 const PORT = process.env.PORT || 8080;
